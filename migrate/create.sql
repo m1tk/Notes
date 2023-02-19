2fe97253fabc2dfce00966b7cd3385c8605e652d
m1tk <mohammed.salih2@usmba.ac.ma>
2023-02-19 22:52:04 +0100

    Add admin account sql file

--- migrate/create.sql
+++ migrate/create.sql
@@ -18,3 +18,7 @@ create table note (
         on delete cascade
         on update restrict
 );
+
+-- Admin account with 123 password
+insert into account (is_admin, email, pass) values (1, "admin@admin.com", "$argon2id$v=19$m=15360,t=2,p=1$niujkTZvDhebTdr20LfZWtUYAnUQfmS4gRpiktIC9ic$4ti0o5q6wvu2x2QEYsLnjKyjpcl3l1l9OO9ecAppUdorwobHumJMeuO7+OAZpIN6YAW2yQ/Aw36wt+ZTcFj5nw");
+
